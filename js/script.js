253c3067df07649eedf8e5287611628539d542b9
JoseS1128 <josekewl35@gmail.com>
2019-11-26 23:23:10 +0000

    added java

--- js/script.js
+++ js/script.js
@@ -5,9 +5,27 @@
 //GLOBAL VARIABLES
 /* global $ */
 $("#shoot").click(function(){;
+    
     let playerChoice = $("#input").val();
     $("#userChoice").text(playerChoice);
+    
+    let Random = Math.random()
 
+    if (Random > .66) {
+        $("#computerChoice").text("rock");
+       
+    } else if (Random < .66 && Random > .33) {
+         $("#computerChoice").text("paper");
+       
+    } else if (Random < .33) {
+         $("#computerChoice").text("scissors");
+        
+    }  else {
+        $("#computerChoice").text("machine broke")
+    }
+
+   
 });
+
 // DOCUMENT READY FUNCTION BELOW
 
