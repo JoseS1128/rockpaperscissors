3fb9c278bb55bc919ec69bebe6eac306e8184ba5
JoseS1128 <josekewl35@gmail.com>
2019-12-10 22:55:49 +0000

    finally done

--- js/script.js
+++ js/script.js
@@ -11,21 +11,55 @@ $("#shoot").click(function(){;
     
     let Random = Math.random()
 
+    let vs = "";
+
     if (Random > .66) {
         $("#computerChoice").text("rock");
-       
+            vs = "rock"; 
     } else if (Random < .66 && Random > .33) {
          $("#computerChoice").text("paper");
-       
+            vs = "paper"; 
     } else if (Random < .33) {
          $("#computerChoice").text("scissors");
-        
+            vs = "scissors"; 
     }  else {
         $("#computerChoice").text("machine broke")
     }
-
    
+    if (vs === "rock" && playerChoice === "rock") {
+        $("#result").text("tie");
+        $("body").css("background-color","yellow");
+    } else if (vs === "paper" && playerChoice === "rock"){
+        $("#result").text("lose");
+        $("body").css("background-color","red");
+    } else if (vs === "scissors" && playerChoice === "rock"){
+        $("#result").text("win");
+        $("body").css("background-color","lawngreen");
+    }
+
+     if (vs === "rock" && playerChoice === "paper") {
+        $("#result").text("win");
+        $("body").css("background-color","lawngreen");
+    } else if (vs === "paper" && playerChoice === "paper"){
+        $("#result").text("tie");
+         $("body").css("background-color","yellow");
+    } else if (vs === "scissors" && playerChoice === "paper"){
+        $("#result").text("lose");
+        $("body").css("background-color","red");
+    }
+
+      if (vs === "rock" && playerChoice === "scissors") {
+        $("#result").text("lose");
+        $("body").css("background-color","red");
+    } else if (vs === "paper" && playerChoice === "scissors"){
+        $("#result").text("win");
+        $("body").css("background-color","lawngreen");
+    } else if (vs === "scissors" && playerChoice === "scissors"){
+        $("#result").text("tie");
+        $("body").css("background-color","yellow");
+    }
 });
+ 
 
 // DOCUMENT READY FUNCTION BELOW
 
